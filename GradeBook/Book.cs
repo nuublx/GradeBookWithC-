e0b175bbf940546bdfbe07150483d83851c42063
nuublx <zeyaddiaa21@gmail.com>
2022-12-14 17:05:35 +0200

    Basic Structure

---
+++ GradeBook/Book.cs
@@ -0,0 +1,39 @@
+﻿namespace GradeBook;
+
+public class Book
+{
+    public Book(string name)
+    {
+        grades = new List<double>();
+        this.name = name;
+        highestGrade = double.MinValue;
+        lowestGrade = double.MaxValue;
+    }
+
+    public bool addGrade(double grade)
+    {
+        if (grade < 0 || grade > 100)
+            return false;
+        lowestGrade = Math.Min(lowestGrade, grade);
+        highestGrade = Math.Max(highestGrade, grade);
+        grades.Add(grade);
+        return true;
+    }
+
+    public void showStatistics()
+    {
+        double averageGrade = 0,sumOfGrades=0;
+        foreach (var grade in grades)
+        {
+            sumOfGrades += grade;
+        }
+        averageGrade = sumOfGrades / grades.Count;
+        Console.WriteLine($"Highest Grade: {highestGrade}");
+        Console.WriteLine($"Lowest Grade: {lowestGrade}");
+        Console.WriteLine($"Average Grade: {averageGrade}");
+    }
+    private List<double> grades;
+    private string name;
+    private double highestGrade;
+    private double lowestGrade;
+}
---
+++ GradeBook/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GradeBook;
+
+Console.WriteLine("GradeBook Project Using C#");
+Book book= new Book("Zeyad's GradeBook");
+book.addGrade(50);
+book.addGrade(80.2);
+book.addGrade(25.52);
+book.addGrade(65.10);
+book.showStatistics();
